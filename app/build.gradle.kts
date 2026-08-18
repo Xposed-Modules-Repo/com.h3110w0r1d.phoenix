ef035aa09c79118e10dd662b727d92481e605a4c
h3110w0r1d <2322013233@qq.com>
2026-08-18 22:44:51 +0800

    :bookmark: v0.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.h3110w0r1d.phoenix"
         minSdk = 26
         targetSdk = 37
-        versionCode = 18
-        versionName = "0.3.3"
+        versionCode = 19
+        versionName = "0.3.4"
     }
 
     splits {
